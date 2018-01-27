ce7cc3e14d8704663eca89c3310b6d565ff27412
jonasrothfuss <jonas.rothfuss@gmx.de>
2018-01-27 11:01:20 +0100

    finished norm learning

--- movement_primitives_optimization/norm_learning.py
+++ movement_primitives_optimization/norm_learning.py
@@ -2,26 +2,10 @@ import numpy as np
 from scipy.optimize import minimize
 import pandas as pd
 from movement_primitives_optimization.helpers import math
+import itertools
 
 
-def inner_minimization_per_dimension(traj_i, traj_j, norm):
-  """
-  Applies Lagrangian optimization (SLSQP method) for the trajectories provided.
-  :param traj_i: First trajectory of shape (time steps of trajectory, )
-  :param traj_j: Second trajectory of shape (time steps of trajectory, )
-  :param norm: A norm under which the optimization process is executed.
-  :return: A scipy OptimizeResult object
-  """
-  fun = lambda traj: ((traj_i-traj).T.dot(norm).dot(traj_i-traj) - math.loss_function(traj, traj_j))
-
-  cons = ({'type': 'eq', 'fun': lambda traj: traj[0] - traj_j[0]},
-          {'type': 'eq', 'fun': lambda traj: traj[-1] - traj_j[1]})
-
-  return minimize(fun, x0=np.ones(shape=(traj_i.shape[0])), method='SLSQP', bounds=None, constraints=cons,
-                  tol=1e-17, options={'ftol': 1e-17, 'disp': True, 'maxiter': 20000, "disp": False})
-
-
-def inner_minimization(traj_i, traj_j, norm):
+def inner_minimization(traj_i, traj_j, M):
   """
   Applies the right term of eq. 19 in "Movement Primitives via Optimization" (Dragan et al., 2015) via Lagrangian
   optimization (SLSQP method with constraints as specified in the paper). Each dimension is optimized separately and
@@ -32,20 +16,47 @@ def inner_minimization(traj_i, traj_j, norm):
   :return: A vector of shape (# dimensions,) that is composed of the min-values of each separate dimension
   Lagrangian-optimization
   """
-  assert traj_i.shape[1] == traj_j.shape[1]
+  assert traj_i.shape == traj_j.shape
+  assert traj_i.ndim == 1, traj_j.ndim == 1
 
-  dimensions = traj_i.shape[1]
-  new_trajectories = []
+  fun = lambda traj: (traj_i - traj).T.dot(M).dot(traj_i - traj) - math.loss_function(traj, traj_j)
 
-  for dim in range(dimensions):
-    new_traj = inner_minimization_per_dimension(traj_i[:, dim], traj_j[:, dim], norm)
-    new_trajectories.append(new_traj.fun)
+  cons = ({'type': 'eq', 'fun': lambda traj: traj[0] - traj_j[0]},
+          {'type': 'eq', 'fun': lambda traj: traj[-1] - traj_j[-1]})
+
+  init_guess = traj_j + np.random.normal(size=(traj_i.shape[0]), scale=0.01)
+
+  opt_result = minimize(fun, x0=init_guess, method='SLSQP', constraints=cons,
+                        options={'maxiter': 20000, "disp": False})
+
+  return opt_result.x, opt_result.fun
 
 
-  return np.asarray(new_trajectories)
+def margin_loss(demonstrations, M):
+  ndim_traj = demonstrations[0].shape[1]
+
+  loss = 0
+  for traj_i, traj_j, dim in itertools.product(demonstrations, demonstrations, range(ndim_traj)):
+    _, inner_min_result = inner_minimization(traj_i[:,dim], traj_j[:,dim], M)
+    loss += (traj_i[:,dim]-traj_j[:,dim]).T.dot(M).dot(traj_i[:,dim]-traj_j[:,dim]) - inner_min_result
+
+  return loss
 
 
-def learn_norm(demonstrations, init_norm, alpha=0.95, iterations=1000):
+def learn_norm_via_opt(demonstrations, init_norm):
+  fun = lambda K: margin_loss(demonstrations, K.T.dot(K))
+
+  print("init_norm shape", init_norm.shape[0])
+
+  #cons = [{'type': 'ineq', 'fun': lambda K: - math.get_d_element(K.T.dot(K),i)} for i in range(init_norm.shape[0])]
+
+
+  opt_result = minimize(fun, x0=init_norm,
+                        options={'maxiter': 20000, "disp": False})
+
+  return opt_result.x
+
+def learn_norm(demonstrations, init_norm, alpha=0.01, iterations=1000):
   """
   Implementation of norm learning from the paper "Movement Primitives via Optimization" (Dragan et al., 2015)
   Specifically, this function learns a norm given that the user provides not only demonstrations but also adaptations
@@ -65,22 +76,35 @@ def learn_norm(demonstrations, init_norm, alpha=0.95, iterations=1000):
   assert alpha > 0
   assert math.is_pos_def(init_norm)
 
+  ndim_traj = demonstrations[0].shape[1]
 
   if isinstance(demonstrations, pd.DataFrame):
     # flatten required to convert 2d array to 1d
     demonstrations = demonstrations.values.flatten()
 
-  norm = init_norm
+
+  M = init_norm
 
 
-  for i in range(iterations):
-    print("iteration: " + str(i+1) + "\n")
-    grad_m = np.zeros(norm.shape)
-    for traj_i in demonstrations:
-      for traj_j in demonstrations:
-        traj_ij = inner_minimization(traj_i, traj_j, norm)
-        grad_m += (traj_i - traj_j).dot((traj_i - traj_j).T) - (traj_i - traj_ij).dot((traj_i - traj_ij).T)
-    norm -= alpha * grad_m
-    # according to eq 20, project M onto the space of pos. def. matrices by polar decomposition
-    norm = math.project_norm_pos_def(norm)
-  return norm
+  def calculate_gradients(traj_i, traj_j, dim):
+    traj_ij, _ = inner_minimization(traj_i[:, dim], traj_j[:, dim], M)
+    grad = (traj_i[:, dim] - traj_j[:, dim]).dot((traj_i[:, dim] - traj_j[:, dim]).T) - (traj_i[:, dim] - traj_ij).dot(
+      (traj_i[:, dim] - traj_ij).T)
+    grads.append(grad)
+
+  for k in range(iterations):
+    grads = []
+    #Parallel(n_jobs=NUM_CORES)(delayed(calculate_gradients)(traj_i, traj_j, dim)
+    #                           for traj_i, traj_j, dim in itertools.product(demonstrations, demonstrations, range(ndim_traj)))
+    for traj_i, traj_j, dim in itertools.product(demonstrations, demonstrations, range(ndim_traj)):
+      calculate_gradients(traj_i, traj_j, dim)
+
+    mean_grad = np.mean(grads)
+    M -= alpha * mean_grad
+
+    M = math.project_norm_pos_def(M)
+    print("LOSS :", margin_loss(demonstrations, M))
+
+  return M
+
+
